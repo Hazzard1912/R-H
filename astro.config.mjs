392247dac0195ee39fcaf3960b92e7b431e5e480
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2024-12-28 10:51:50 -0500

    Add custom pages to sitemap integration and create sitemap files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,17 @@ import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel";
 import sitemap from '@astrojs/sitemap';
 
+const customPages = [
+  '/tags/long_hair/1',
+  '/tags/highres/1',
+  '/tags/underwear/1',
+  '/image/1',
+  '/image/3',
+  '/image/4',
+  '/image/5',
+  '/image/6',
+]
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -13,5 +24,7 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap({
+    customPages: customPages.map(page => `https://rinconhentai.com${page}`)
+  })]
 });
