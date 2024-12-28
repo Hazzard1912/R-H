0c018efd4db627ee6c0c0d45400a7bb900341504
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2024-12-28 10:24:36 -0500

    Add sitemap integration and set site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://rinconhentai.com",
   adapter: vercel(),
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
